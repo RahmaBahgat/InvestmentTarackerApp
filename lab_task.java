418b577014ad5c67ca24cc50b0046f498199829d
bassant2005 <basant.tareq.2005@gmail.com>
2025-05-07 19:05:53 +0300

    commit 1

--- lab_task.java
+++ lab_task.java
@@ -1,6 +1,7 @@
 public class lab_task {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("20231037 - bassant tarek ");
     }
 }
 
