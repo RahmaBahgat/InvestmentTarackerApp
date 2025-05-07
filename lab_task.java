3867130435cc2976d4eb5cf2333be719463136da
bassant2005 <basant.tareq.2005@gmail.com>
2025-05-07 19:19:29 +0300

    delete lab task

--- lab_task.java
+++
@@ -1,9 +0,0 @@
-public class lab_task {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("20231037");
-        System.out.println("20231067");
-        System.out.println(" ");
-    }
-}
-
