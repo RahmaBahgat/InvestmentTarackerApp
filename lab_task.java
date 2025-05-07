9258adebc992d13f99ae889c170d9c5589021ca2
bassant2005 <basant.tareq.2005@gmail.com>
2025-05-07 19:09:35 +0300

    20231067

--- lab_task.java
+++ lab_task.java
@@ -2,7 +2,7 @@ public class lab_task {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("20231037 - bassant tarek ");
-        System.out.println("20231067 - rawda raafat ");
+        System.out.println("20231067 ");
         System.out.println("20231056");
     }
 }
